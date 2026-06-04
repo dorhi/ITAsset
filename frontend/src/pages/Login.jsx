4e445492a32bd055de4c305591adedaa2cd79926
dorhi <koindol.ssts@gmail.com>
2026-06-04 18:25:38 +0900

    login 수정

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
   return (
     <div className="login-container">
       <div className="login-card">
-        <h2>IT 자산 관리 로그인2222</h2>
+        <h2>IT 자산 관리 로그인</h2>
         <p>계정 정보를 입력해주세요</p>
         
         {error && <div className="error-msg">{error}</div>}
